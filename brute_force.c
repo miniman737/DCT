b7f256dce4ece2972060c86ff14cca03a39203e0
Marcus Ganz <marcusganz737@gmail.com>
2026-07-24 18:13:26 -0700

    more progress on brute DCT

--- brute_force.c
+++ brute_force.c
@@ -10,36 +10,39 @@ int main() {
 
 // this is the brute force naive solution as provided by
 // Mihai SIMA in slide deck 4 of the SENG440 course notes
-void 2D_IDCT(int * input_stream, int * output_stream) {
+void 2D_DCT(uint16_t * input_stream, uint16_t * output_stream) {
   // input and output for the system
-  int i_block[8][8], o_block[8][8];
-  int t1_block[8][8], t2_block[8][8];
+  uint16_t i_block[8][8], o_block[8][8];
+  uint16_t t1_block[8][8], t2_block[8][8];
   
   // counter values for the loops
   int k, l;
 
   for (k = 0; k < 8; k++) {
     for(l = 0; l < 8; l++) {
-      i_block[k][l] = input_stream[8*k+l];
+      i_block[k][l] = input_stream[8 * k + l];
     }
   }
 
   for (k=0; k < 8; k++) {
-    idct(i_block[k], t1_block[k]);
+    dct(i_block[k], t1_block[k]);
   }
 
   transposition(t1_block, t2_block);
 
   for (k=0; k < 8; k++) {
-    idct(t2_block, o_block[k]);
+    dct(t2_block[k], o_block[k]);
   }
 
   for (k=0; k < 8; k++) {
-    output_stream[8*k+1] = o_block[k][l];
+    output_stream[8 * k + l] = o_block[k][l];
   }
 }
 
-void idct(){
+void dct(int * input, int * output){
+  uint_t n, k;
+
+
 }
 
 void transposition() {}
